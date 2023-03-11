aabff73e3f67bce4befe3c8fdfe12cdd146f00b5
ren_mcc <ren_mcc@foxmail.com>
2023-03-11 23:11:35 +0800

    更新响应体格式

--- http/response/data.go
+++ http/response/data.go
@@ -19,8 +19,8 @@ type Message struct {
 	Namespace string      `json:"namespace,omitempty"`  // 异常的范围
 	Reason    string      `json:"reason,omitempty"`     // 异常原因
 	Recommend string      `json:"recommend,omitempty"`  // 推荐链接
-	Describe  string      `json:"message,omitempty"`    // 关于这次响应的说明信息
-	Message   interface{} `json:"data,omitempty"`       // 返回的具体数据
+	Describe  string      `json:"describe,omitempty"`   // 关于这次响应的说明信息
+	Message   interface{} `json:"message,omitempty"`    // 返回的具体数据
 	Meta      interface{} `json:"meta,omitempty"`       // 数据meta
 }
 
